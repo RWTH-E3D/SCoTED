8d0535ed32bdfa66d5c7636bfaba51e6d35488f4
LENOVO-PF3BSEAY\Cuypers <cuypers@e3d.rwth-aachen.de>
2022-10-28 15:25:55 +0200

    Preliminary program structure

---
+++ scoted.py
@@ -0,0 +1,15 @@
+
+
+class SCoTED(object):
+
+    def __init__(self):
+        pass
+
+    def generate_heating_load_curve(self):
+        pass
+
+    def generate_dhw(self)
+        pass
+
+    def gernerate_energy_consumption_curve(self):
+        pass
