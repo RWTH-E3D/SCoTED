f026ab53da92fe23805092f2efabe90662d2b616
LENOVO-PF3BSEAY\Cuypers <cuypers@e3d.rwth-aachen.de>
2022-11-02 13:43:28 +0100

    Added documentation

--- SCoTED/scoted.py
+++ SCoTED/scoted.py
@@ -6,9 +6,7 @@ class SCoTED(object):
 
     def __init__(self):
         self._weather = None
-        self.heat_load_12831 = None
-        self.t_standard_12831 = None
-        self.t_heating_limit = None
+
 
 
     @property
@@ -26,23 +24,32 @@ class SCoTED(object):
 
         self._weather = weather
 
-    def generate_heating_load_curve(self):
+    def generate_heating_load_curve(self, heat_load_12831, t_standard_12831, t_heating_limit):
+        """This function calculates the annual heating load curve from ambient temperature. For this purpose, it
+        interpolates linearly between the standard heating load and its standard temperature and the heating limit
+        temperature and 0°C.
+
+        Parameters
+        ----------
+        heat_load_12831: numeric
+            Standard heating load according to DIN EN 12831
+        t_standard_12831: numeric
+            standard temperature according to DIN EN 12831
+        t_heating_limit: numeric
+            Heating limit temperature
+        Returns
+        -------
+        heating_load_curve : np.array
+            Heating load curve in the exact resolution as the weather data stored in the object.
+
+        """
 
         weather = self.weather.to_numpy()
 
-        heating_load_curve = self.heat_load_12831 * ((self.t_heating_limit-weather[:, 1])/(self.t_heating_limit - self.t_standard_12831))
+        heating_load_curve = heat_load_12831 * ((t_heating_limit-weather[:, 1])/(t_heating_limit - t_standard_12831))
         heating_load_curve = heating_load_curve.clip(min = 0)
 
         return weather[:, 0], heating_load_curve
-
-
-
-
-
-
-
-
-
 
     def generate_dhw_curve(self):
         pass
