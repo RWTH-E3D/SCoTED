84f47fdae8f15fd167bd29b946c59b0e45767ba7
Julian Körner <julian.koerner@rwth-aachen.de>
2023-06-21 11:57:57 +0200

    Added a json file containing 3 different 24h domestic hot water (dhw) profiles/ tap profiles from the DIN EN 15450 and 3 more methods for creating hourly load profiles. One for domestic hot water (dhw), one for maximum cooling load according to Norm and one for the annual energy consumption for cooling. Input for the dhw method can either be the energy consumption for dhw itself or the overall heating consumption together with the percentage of dhw and the name for the dhw_profile to be used can be chosen between 'average_single_person', 'average_family' and 'average_threeheaded_family' (default is 'average_single_person'). The method for cooling load takes the standard cooling load (VDI 2078), the temperature belonging to the maximum cooling load (t_standard) and the cooling limit temperature (t_cooling_limit; outdoor temperature at which cooling starts) as input arguments. For the cooling consumption method the cooling consumption is required together with the cooling limit temperature as in the cooling load method.

--- SCoTED/scoted.py
+++ SCoTED/scoted.py
@@ -1,12 +1,23 @@
 import numpy
 import numpy as np
 import pandas as pd
+import json
+import os
 
 
 class SCoTED(object):
 
     def __init__(self):
         self._weather = None
+        self._dhw_profiles = self.load_dhw_profiles()
+
+    @staticmethod
+    def load_dhw_profiles():
+        dhw_profiles_path = os.path.join(os.path.abspath(os.curdir), "..", "tests", "testdata",
+                                         "dhw_profiles.json")
+        with open(dhw_profiles_path) as f:
+            dhw_profiles = json.load(f)
+        return dhw_profiles
 
     @property
     def weather(self):
@@ -82,11 +93,115 @@ class SCoTED(object):
 
         return curve
 
-    def generate_dhw_curve(self, test):
+    def generate_dhw_curve(self, heating_consumption_dhw=None, heating_consumption=None, percentage_dhw=None,
+                           dhw_profile_name=None):
+        """This function calculates the hourly domestic hot water (dhw) profile based on a given annual energy/ heating
+        consumption for dhw (or based on a given annual heating consumption and the percentage of dhw) and
+        a 24h tap profile (dhw profile) located in a json file.
 
-        pass
+        Parameters
+        ----------
+        heating_consumption_dhw: numeric
+            Annual energy consumption for dhw [Wh].
+        heating_consumption: numeric
+            Annual energy consumption for heating the building [Wh]
+        percentage_dhw: numeric
+            Percentage of dhw in annual energy consumption in decimal [-]
+        dhw_profile_name: string
+            Name of the dhw profile to be used. Default profile is "average_single_person", which is taken from the
+            DIN 15450. The other profiles available are "average_family" and "average_threeheaded_family"
 
-    def gernerate_heating_consumption_curve(self, heating_consumption, t_standard_12831, t_heating_limit):
+        Returns
+        -------
+        numpy.array
+            Generated array with hourly (8760) values for dhw energy consumption [W]
+        """
+
+        if heating_consumption and percentage_dhw is not None:
+            load_value = heating_consumption * percentage_dhw
+        elif heating_consumption_dhw is not None:
+            load_value = heating_consumption_dhw
+        else:
+            raise Exception("Either a value for the dhw heating consumption must be given, or in case"
+                            "of a given total energy consumption, the percentage of dhw is needed.")
+
+        if dhw_profile_name is not None:
+            try:
+                used_profile = np.array(self._dhw_profiles["dhw_profiles"][dhw_profile_name])
+            except Exception:
+                raise Exception("No viable dhw profile name was passed. Available profiles:\n"
+                                "'average_single_person', 'average_family', 'average_threeheaded_family'\n"
+                                "If no profile name is given, default profile 'average_single_person' is used")
+        else:
+            used_profile = np.array(self._dhw_profiles["dhw_profiles"]["average_single_person"])
+
+        sum_over_year = sum(used_profile) * 365
+        dhw_norm_day = used_profile / sum_over_year
+        dhw_profile = load_value * dhw_norm_day
+
+        return np.tile(dhw_profile, 365)
+
+    def generate_cooling_load_curve(self, cooling_load_2078, t_standard, t_cooling_limit):
+        """This function calculates the annual cooling load curve from ambient temperature. For this purpose, it
+        interpolates linearly between the standard cooling load and its standard temperature and the cooling limit
+        temperature and 0W.
+
+        Parameters
+        ----------
+        cooling_load_2078 : numeric
+            Standard cooling load according to VDI 2078 [W]
+        t_standard : numeric
+            Standard temperature belonging to the cooling load [°C]
+        t_cooling_limit : numeric
+            cooling limit temperature [°C]
+        Returns
+        -------
+        cooling_load_curve : numpy array
+            Cooling load curve in the exact resolution as the weather data stored in the object. [W]
+
+        """
+
+        reference_point1 = np.array([cooling_load_2078, t_standard])
+        reference_point2 = np.array([0, t_cooling_limit])
+
+        cooling_load_curve = self._curve_generator(reference_point1, reference_point2, temperature=None)
+        cooling_load_curve.clip(min=0, out=cooling_load_curve)
+
+        return self.weather[:, 0], cooling_load_curve
+
+    def generate_cooling_consumption_curve(self, cooling_consumption, t_cooling_limit):
+        """This function calculates the annual cooling energy consumption curve from the ambient temperature.
+
+        Parameters
+        ----------
+        cooling_consumption : numeric
+            Annual energy consumption for heating the building [Wh]
+        t_cooling_limit : numeric
+            Cooling limit temperature [°C]
+        Returns
+        -------
+        cooling_load_curve : numpy array
+            Cooling load curve in the exact resolution as the weather data stored in the object. [W]
+        """
+        if self.weather is None:
+            raise ValueError("Please specify a weather dataset in the object")
+        else:
+            temperature = self.weather[:, 1]
+
+        cooling_hours = (temperature - t_cooling_limit) * 10  # for 0.1 degree C steps
+        sum_cooling_hours = np.sum(cooling_hours[cooling_hours > 0])
+        b = cooling_consumption / sum_cooling_hours  # [W/0.1°C]
+
+        cooling_load_curve = np.zeros(8760)
+        for i in range(8760):
+            if cooling_hours[i] >= 0:
+                cooling_load_curve[i] = b * 10 * (temperature[i] - t_cooling_limit)
+        # cooling_load_curve = b * 10 * (temperature - t_cooling_limit)
+        cooling_load_curve.clip(min=0, out=cooling_load_curve)
+
+        return self.weather[:, 1], cooling_load_curve
+
+    def generate_heating_consumption_curve(self, heating_consumption, t_standard_12831, t_heating_limit):
         """This function calculates the annual heating energy consumption curve from the ambient temperature.
 
 
